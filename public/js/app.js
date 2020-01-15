70f17a77097fbaac76290a504b13a1868d08506a
kuzmich321 <dota2drot@gmail.com>
2020-01-15 13:14:20 +0300

    Seeder added and a bunch of dummy data  as well

--- public/js/app.js
+++ public/js/app.js
@@ -19359,8 +19359,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/andrew/WebDev/PhpstormProjects/laravel-blog/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/andrew/WebDev/PhpstormProjects/laravel-blog/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/andrew/WebDev/PhpstormProjects/laravel-blog-dev/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/andrew/WebDev/PhpstormProjects/laravel-blog-dev/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
