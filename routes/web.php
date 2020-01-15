bfe58bbd2817c4dc3fbafd18a608ab840e4411f8
kuzmich321 <dota2drot@gmail.com>
2020-01-15 20:00:42 +0300

    Routes expanded

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/user/{id}', 'UserController@show');
+
+Route::get('/users', 'UserController@index');
