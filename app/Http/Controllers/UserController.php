da28f7dee0b1f1955a3f3588bf1e3bb2f25bc15b
kuzmich321 <dota2drot@gmail.com>
2020-01-16 17:30:21 +0300

    extra enter added to make space between code and return

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,6 +15,7 @@ class UserController extends Controller
     public function index()
     {
         $users = User::all();
+
         return view('users', [
             'users' => $users
         ]);
