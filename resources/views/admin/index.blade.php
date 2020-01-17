5e0bfb027d31df1674f64b718b00974d42ca645e
kuzmich321 <dota2drot@gmail.com>
2020-01-17 09:34:32 +0300

    Admin UserController, Admin index added

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <table class="table table-striped table-hover">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($users as $user)
+                <tr onclick="window.location='{{ route("users.show", [$user->id]) }}'" style="cursor: pointer">
+                    <th scope="row">{{$user->id}}</th>
+                    <td>{{$user->name}}</td>
+                    <td>{{$user->email}}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+        <div class="pagination-wrapper d-flex justify-content-center">
+            {{ $users->links() }}
+        </div>
+    </div>
+@endsection
