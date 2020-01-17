15bf61801b8be51d2218846fc0e52513547a8622
kuzmich321 <dota2drot@gmail.com>
2020-01-17 09:28:29 +0300

    Admin UserController, Admin index, Admin routes added

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('users', 'UserController')->only([
     'index', 'show'
 ]);
+
+Route::prefix('admin')->group(fn() => Route::resource('users', 'Admin\UserController'));
