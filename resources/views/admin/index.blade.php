fd4b19be5043ef02708b344eabd8299de4373124
kuzmich321 <dota2drot@gmail.com>
2020-01-18 11:57:09 +0300

    user passed instead of user->id

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -12,7 +12,7 @@
             </thead>
             <tbody>
             @foreach($users as $user)
-                <tr onclick="window.location='{{ route("admin.users.edit", [$user->id]) }}'" style="cursor: pointer">
+                <tr onclick="window.location='{{ route("admin.users.edit", [$user]) }}'" style="cursor: pointer">
                     <th scope="row">{{$user->id}}</th>
                     <td>{{$user->name}}</td>
                     <td>{{$user->email}}</td>
