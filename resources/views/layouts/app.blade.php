2777073e39ab76d493fcbf18f9f5b40c4f92e2de
kuzmich321 <dota2drot@gmail.com>
2020-01-19 16:49:05 +0300

    session status transferred to app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,11 @@
 </head>
 <body>
     <div id="app">
+        @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
+            </div>
+        @endif
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
