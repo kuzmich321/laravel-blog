457aea872505d1c35f6d81266369581dc70f2fab
kuzmich321 <dota2drot@gmail.com>
2020-01-21 20:20:35 +0300

    small fix (unnecessary form removed)

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -4,16 +4,14 @@
     <div class="container">
         <div class="posts-wrapper w-90 m-auto">
             @foreach($posts as $post)
-                <div class="card bg-transparent border-dark mb-3"
-                     style="cursor: pointer">
+                <div class="card bg-transparent border-dark mb-3">
                     <div class="card-body text-dark">
                         <h5 class="card-title">{{ $post->title }}</h5>
                         <p class="card-text">{{ $post->description }}</p>
                         <div class="buttons float-right">
                             <button type="submit"
                                     class="btn btn-info"
-                                    form="posts-edit"
-                                    formaction="{{ route('admin.posts.edit', $post) }}">Edit
+                                    onclick="window.location='{{ route('admin.posts.edit', $post) }}'">Edit
                             </button>
                             <button class="btn btn-danger">Delete</button>
                             <button class="btn btn-primary">Restore</button>
@@ -22,7 +20,6 @@
                 </div>
             @endforeach
         </div>
-        <form id="posts-edit" method="GET"></form>
         <div class="pagination-wrapper d-flex justify-content-center">
             {{ $posts->links() }}
         </div>
