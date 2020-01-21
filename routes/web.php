3be48763a8fd52fcd585893280e43add67c004e0
kuzmich321 <dota2drot@gmail.com>
2020-01-21 12:44:52 +0300

    auth middleware attached to the group of admin routes

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::resource('users', 'UserController')->only([
     'index', 'show'
 ]);
 
-Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function () {
+Route::namespace('Admin')->prefix('admin')->middleware('auth')->name('admin.')->group(function () {
     Route::patch('users/{user}/restore', 'UserController@restore')->name('users.restore');
     Route::resource('users', 'UserController');
 });
