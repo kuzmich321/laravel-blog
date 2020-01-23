0754ad290710a1952e6969c1638f744836209d1b
kuzmich321 <dota2drot@gmail.com>
2020-01-23 11:10:40 +0300

    relationships inserted into views

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,10 +2,12 @@
 
 @section('content')
     <div class="container">
-        <div class="card">
+        <div class="card bg-transparent border border-dark">
             <div class="card-body">
                 <h5 class="card-title">{{ $post->title }}</h5>
                 <p class="card-text">{{ $post->description }}</p>
+                <a href="{{ route('users.show', $post->user_id) }}"
+                   class="card-link">{{ $post->user->name }}</a>
             </div>
         </div>
     </div>
