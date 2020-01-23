8bd50d4574278202b090f48acad853d8442de188
kuzmich321 <dota2drot@gmail.com>
2020-01-23 11:33:41 +0300

    files renamed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,20 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\User;
-use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index()
     {
         $users = User::all();
 
-        return view('users', [
+        return view('index', [
             'users' => $users
         ]);
     }
@@ -24,12 +24,12 @@ class UserController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param \App\User $user
-     * @return \Illuminate\Http\Response
+     * @param User $user
+     * @return Response
      */
     public function show(User $user)
     {
-        return view('singleUser', [
+        return view('show', [
             'user' => $user
         ]);
     }
