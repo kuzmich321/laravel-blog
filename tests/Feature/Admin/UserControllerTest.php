13c0462de4cbf9ee22e8bcf1c3446e2af6765821
kuzmich321 <dota2drot@gmail.com>
2020-01-27 13:23:58 +0300

    Added user tests for edit, update, create, and store

--- tests/Feature/Admin/UserControllerTest.php
+++ tests/Feature/Admin/UserControllerTest.php
@@ -4,11 +4,13 @@ namespace Tests\Feature\Admin;
 
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class UserControllerTest extends TestCase
 {
     use RefreshDatabase;
+    use WithFaker;
 
     /**
      * @test
@@ -40,5 +42,97 @@ class UserControllerTest extends TestCase
         $response->assertOk()
             ->assertViewIs('admin.show')
             ->assertViewHas('user');
+    }
+
+    /**
+     * @test
+     */
+    public function testEdit()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)
+            ->get(route('admin.users.edit', $user));
+
+        $response->assertOk()
+            ->assertViewIs('admin.edit')
+            ->assertViewHas('user');
+    }
+
+    /**
+     * @test
+     */
+    public function testUpdate()
+    {
+        $user = factory(User::class)->create();
+
+        $url = route('admin.users.update', $user);
+
+        $password = $this->faker->password();
+
+        $formData = [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->email(),
+            'password' => $password,
+            'password_confirmation' => $password
+        ];
+
+        $response = $this->actingAs($user)
+            ->patch($url, $formData);
+
+        $response->assertRedirect(route('admin.users.show', $user));
+
+        $response->assertSessionHas('status', __('statuses.users.updated'));
+
+        $this->assertDatabaseHas('users', [
+            'id' => $user->id,
+            'name' => $formData['name'],
+            'email' => $formData['email']
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function testCreate()
+    {
+        $admin = factory(User::class)->create();
+
+        $response = $this->actingAs($admin)
+            ->get(route('admin.users.create'));
+
+        $response->assertOk()
+            ->assertViewIs('admin.create');
+    }
+
+    /**
+     * @test
+     */
+    public function testStore()
+    {
+        $admin = factory(User::class)->create();
+
+        $url = route('admin.users.store');
+
+        $password = $this->faker->password();
+
+        $formData = [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->email(),
+            'password' => $password,
+            'password_confirmation' => $password
+        ];
+
+        $response = $this->actingAs($admin)
+            ->post($url, $formData);
+
+        $response->assertRedirect(route('admin.users.index'));
+
+        $response->assertSessionHas('status', __('statuses.users.created'));
+
+        $this->assertDatabaseHas('users', [
+            'name' => $formData['name'],
+            'email' => $formData['email']
+        ]);
     }
 }
