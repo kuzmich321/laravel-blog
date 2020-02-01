3a675737e5ef9ea7237d1546c1f7bb157e31cb55
kuzmich321 <dota2drot@gmail.com>
2020-02-01 11:44:45 +0300

    posts api added

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Post;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $postQuery = Post::query();
+
+        if (request('by_user_id')) {
+            $userId = request('by_user_id');
+            $postQuery->where('user_id', $userId);
+        }
+
+        $posts = $postQuery->get();
+
+        return $posts;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param int $id
+     * @return Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
