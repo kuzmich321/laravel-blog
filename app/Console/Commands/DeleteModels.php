f6dcda809126911b58c6a83ef8a5becba43d25bc
kuzmich321 <dota2drot@gmail.com>
2020-02-07 15:48:27 +0300

    constructor removed

--- app/Console/Commands/DeleteModels.php
+++ app/Console/Commands/DeleteModels.php
@@ -19,16 +19,6 @@ class DeleteModels extends Command
     protected $signature = 'models:force-delete';
 
     /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    /**
      * Execute the console command.
      *
      * @return mixed
